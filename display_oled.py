fefe2a03517132b53bcff93697294cf6a5a7ff71
hscot <59375768+hscot@users.noreply.github.com>
2020-04-25 15:27:01 -0500

    Add files via upload

--- display_oled.py
+++ display_oled.py
@@ -66,9 +66,9 @@ while True:
 
         #Begin display information
         draw.text((x, top + 0), (first_c), font=font, fill=255)
-        draw.text((x, top + 8), ("Infected ") + (infections_c), font=font, fill=255)
-        draw.text((x, top + 16), ("Worldwide"), font=font, fill=255)
-        draw.text((x, top + 24), ("Infected ") + (infections_w), font=font, fill=255)
+        draw.text((x, top + 8), "Infected " + (infections_c) " +" (today_c), font=font, fill=255)
+        draw.text((x, top + 16), "Worldwide", font=font, fill=255)
+        draw.text((x, top + 24) "Infected " + (infections_w) " +" (today_w), font=font, fill=255)
         disp.image(image)
         disp.show()
         time.sleep(10)
@@ -88,7 +88,7 @@ while True:
         draw.text((x, top + 0), (first_c), font=font, fill=255)
         draw.text((x, top + 8), "Recovered" + (survived_c), font=font, fill=255)
         draw.text((x, top + 16), "Worldwide", font=font, fill=255)
-        draw.text((x, top + 24), (survived_w), font=font, fill=255)
+        draw.text((x, top + 24) (survived_w), font=font, fill=255)
         disp.image(image)
         disp.show()
         time.sleep(10)
---
+++ loop-script.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+while true; do
+    # Because I haven't updated my git repository, I dont have the proper filenames.
+    # Implement this with the proper file names
+    #python3 cleardisplay_oled.py
+    python3 display_oled.py
+    sleep 10m
+    #python3 cleardisplay_oled.py
+    python3 display_oled.py
+done
